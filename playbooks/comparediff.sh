f7d1294ca457929fd5c157fd3b877b086cf8c93d
naqeebghazi <naqeebghazi@outlook.com>
2023-12-19 23:57:37 +0000

    images

--- playbooks/comparediff.sh
+++ playbooks/comparediff.sh
@@ -1,21 +1,21 @@
-#!/bin/bash
+# #!/bin/bash
 
-file1="$1"
-file2="$2"
+# file1="$1"
+# file2="$2"
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 $file1 $file2"
-    exit 1
-fi
+# if [ "$#" -ne 2 ]; then
+#     echo "Usage: $0 $file1 $file2"
+#     exit 1
+# fi
 
-if [ ! -f "$file1" ]; then
-    echo "Error: The File '$file1' does not exist."
-    exit 1
-fi
+# if [ ! -f "$file1" ]; then
+#     echo "Error: The File '$file1' does not exist."
+#     exit 1
+# fi
 
-if [ ! -f "$file2" ]; then
-    echo "Error: The File '$file2' does not exist."
-    exit 1
-fi
+# if [ ! -f "$file2" ]; then
+#     echo "Error: The File '$file2' does not exist."
+#     exit 1
+# fi
 
-diff "$file1" "$file2"
+# diff "$file1" "$file2"
